8b87481815a9cb4a566124fc22e55cbcb29e42cf
akanadirensg <nadir.akayab@ensg.eu>
2025-11-15 15:54:07 +0100

    Final commit (ajout de tests pour  EnvelopeBuilder)

--- test/Envelope.spec.ts
+++ test/Envelope.spec.ts
@@ -2,6 +2,9 @@ import "mocha";
 import { expect } from "chai";
 import Envelope from "../src/Envelope";
 import EnvelopeBuilder from "../src/EnvelopeBuilder";
+import Point from "../src/Point";
+import LineString from "../src/LineString";
+import GeometryCollection from "../src/GeometryCollection";
 
 describe("test Envelope", () => {
     it("test envelope avec envelopeBuilder", () => {
@@ -39,6 +42,57 @@ describe("test Envelope", () => {
         expect(Number.isNaN(envelope_vide.getXMin()));
         expect(Number.isNaN(envelope_vide.getYMin()));
         expect(envelope_vide.toString()).to.equal("");
+    });
+    it("test envelope avec Point", () => {        
+        const builder = new EnvelopeBuilder();
+        const p=new Point([0.0,1.0])
+        const p1=new Point([2.0,0.0])
+        const p2=new Point([1.0,3.0])
+
+        p.accept(builder)
+        const result = builder.build();
+
+        expect(result.getXMax()).to.equal(0.0)
+        expect(result.getYMax()).to.equal(1.0)
+        expect(result.getXMin()).to.equal(0.0)
+        expect(result.getYMin()).to.equal(1.0)
+        
+    });
+    it("test envelope avec LineString", () => {        
+        const builder = new EnvelopeBuilder();
+        const p=new Point([0.0,1.0])
+        const p1=new Point([2.0,0.0])
+        const p2=new Point([1.0,3.0])
+
+        const l=new LineString([p,p1,p2])
+
+        l.accept(builder)
+        const result = builder.build();
+
+        expect(result.getXMax()).to.equal(2.0)
+        expect(result.getYMax()).to.equal(3.0)
+        expect(result.getXMin()).to.equal(0.0)
+        expect(result.getYMin()).to.equal(0.0)
+        
+    });
+    it("test envelope avec LineString", () => {        
+        const builder = new EnvelopeBuilder();
+        const p=new Point([0.0,1.0])
+        const p1=new Point([2.0,0.0])
+        const p2=new Point([1.0,3.0])
+
+        const l=new LineString([p,p1,p2])
+
+        const g=new GeometryCollection([l,p,p1,p2])
+
+        g.accept(builder)
+        const result = builder.build();
+
+        expect(result.getXMax()).to.equal(2.0)
+        expect(result.getYMax()).to.equal(3.0)
+        expect(result.getXMin()).to.equal(0.0)
+        expect(result.getYMin()).to.equal(0.0)
+        
     });
 
 });
